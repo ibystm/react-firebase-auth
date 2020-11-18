3beda9cb87329a652eee6a5bbca3d1cfa7282c2a
Kosuke Takahshi <kosuketakahashi@KosukenoMacBook-Pro.local>
2020-11-19 01:44:50 +0900

    first commit

---
+++ src/store/types.ts
@@ -0,0 +1,39 @@
+export const SET_USER = 'SET_USER';
+export const SIGN_OUT = 'SIGN_OUT';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ERROR = 'SET_ERROR';
+export const NEED_VERIFICATION = 'NEED_VERIFICATION';
+export const SET_SUCCESS = 'SET_SUCCESS';
+
+export interface User {
+  firstName: string;
+  email: string;
+  id: string;
+  createdAt: any;
+}
+
+export interface AuthState {
+  user: User | null;
+  authenticated: boolean;
+  loading: boolean;
+  error: string;
+  needVerification: boolean;
+  success: string;
+}
+
+export interface SignUpDate {
+  firstName: string;
+  email: string;
+  password: string;
+}
+
+export interface SignInData {
+  email: string;
+  password: string;
+}
+
+// Actions
+interface SetUserAction {
+  type: typeof SET_USER;
+  payload: User;
+}
