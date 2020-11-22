f2f0d79be666083596e759ad9be309f3caefdffa
Kosuke Takahshi <kosuketakahashi@KosukenoMacBook-Pro.local>
2020-11-22 17:20:36 +0900

    added route management component

---
+++ src/components/auth/PublicRoute.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { Redirect, Route, RouteProps } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+
+interface Props extends RouteProps {
+  component: any;
+}
+
+// PrivateRouteComponentでは、compont propだけ受け取り、残りは子コンポーネントに渡す
+const PublicRoute: FC<Props> = ({ component: Component, ...rest }) => {
+  //  useSelectorすることで、connectしてないcomponentでもStoreのstateを参照することができる
+  const { authenticated } = useSelector((state: RootState) => state.auth);
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        !authenticated ? <Component {...props} /> : <Redirect to="/signIn" />
+      }
+    />
+  );
+};
+
+export default PublicRoute;
